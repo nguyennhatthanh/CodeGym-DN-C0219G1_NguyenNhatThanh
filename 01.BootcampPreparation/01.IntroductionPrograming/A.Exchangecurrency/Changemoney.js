3d4cdba488a0d4656c27876176a42c43382817ec
Linez94 <nguyennhatthanh3394@gmail.com>
2019-02-22 19:26:39 +0700

    finalupdate_22022019

--- 01.BootcampPreparation/01.IntroductionPrograming/A.Exchangecurrency/Changemoney.js
+++ 01.BootcampPreparation/01.IntroductionPrograming/A.Exchangecurrency/Changemoney.js
@@ -1,3 +1,7 @@
-function Myfunction(){
-    document.getElementByIds
+function changeMoney(){
+    var outputMoney = 0;
+    var inputMoney = parseInt(document.getElementById("inputMoney").value);
+    
+
+    document.getElementById("outputMoney").value = document.getElementById("fromMoney").value;
 }
