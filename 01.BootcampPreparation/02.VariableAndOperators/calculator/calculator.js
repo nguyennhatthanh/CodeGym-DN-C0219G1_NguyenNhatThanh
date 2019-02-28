b05b0f6495dce127b17e79394eb5d5d9eb71187a
nguyennhatthanh <nguyennhatthanh3394@gmail.com>
2019-02-28 11:48:49 +0700

    new update

---
+++ 01.BootcampPreparation/02.VariableAndOperators/calculator/calculator.js
@@ -0,0 +1,7 @@
+var a = parseInt(document.getElementById("Number1").value);
+console.log(a);
+var b = parseInt(document.getElementById("Number2").value);
+console.log(b)
+function cong(){
+    return a+b;
+}
